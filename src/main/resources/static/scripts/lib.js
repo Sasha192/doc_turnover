dc2bd1ec792140d7295ef8a88c7ec4293ddd04de
Sasha192 <sasha192.bunin@gmail.com>
2020-05-02 13:22:14 +0300

    nav-bar modif

--- src/main/resources/static/scripts/lib.js
+++ src/main/resources/static/scripts/lib.js
@@ -4,22 +4,22 @@ function log () {
 	}
 }
 
-const relSelector = ( obj1, obj2 ) => {
+function relSelector ( obj1, obj2 ) {
 	return obj1.querySelector( obj2 )
 }
 
-const relSelectorAll = ( obj1, obj2 ) => {
+function relSelectorAll ( obj1, obj2 ) {
 	return obj1.querySelectorAll( obj2 );
 }
 
-const selector = (obj) => {
+function selector (obj) {
 	return document.querySelector( obj )
 }
 
-const createElem = (elem) => {
+function createElem (elem) {
 	return document.createElement( elem )
 }
 
-const createText = (text) => {
+function createText (text) {
 	return document.createTextNode( text )
 }
--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -1,13 +1,10 @@
-// FILTER //
-
 $.ajax({
   url: "/departments",
   type: "GET",
   contentType: "application/json",
   success: function (data) {
-    let departments = JSON.parse(data);
-    console.log(data);
-    loadData(departments);
+    log(data);
+    loadData(data);
   },
 });
 
@@ -21,10 +18,11 @@ function loadData(data) {
     depCell.appendChild(option);
   }
   let selected;
-
-  depCell.onchange = () => {
+  depCell.onchange = function () {
     selected = depCell.options[depCell.selectedIndex];
+
     let empCell = selector("#filter-employee");
+
     if (selected.getAttribute("value")) {
       let index = selected.getAttribute("value");
       let employees = data[index].employees;
@@ -49,44 +47,146 @@ function loadData(data) {
   };
 }
 
-// ARHIVE //
+// archive
 
 $.ajax({
-  url: "/main-arhive/data",
+  url: "/doc/list",
   type: "GET",
   contentType: "application/json",
   success: function (data) {
-    let arhive = JSON.parse(data);
-    loadArhive(arhive);
+    loadArchive(data);
+  },
+  error: function (req, text, error) {
+    console.log("Ошибка: " + text + " | " + error);
   },
 });
 
-function loadArhive(data) {
+function loadArchive(data) {
+  console.log(data);
+  let archive = selector(".arсhive-table tbody");
   for (let i = 0; i < data.length; i++) {
     let row = createElem("tr");
-		row.className = "arhive-row";
+    row.className = "archive-row";
+    let obj = data[i];
+      row.innerHTML = `
+        <td><a>${obj.name}</a></td>
+        <td>${obj.creationDate}</td>
+        <td><a>${obj.performer.department.departmentName}</a></td>
+        <td><a>${obj.performer.mame}</a></td>      
+        <td></td>
+    `;
 
-		let name = createElem("td").innerHTML = '<span class="'data[i].priority'">'data[i].name'</span>';
-		let date = createElem("td").innerHTML = data[i].date;
-		let department = createElem("td").innerHTML = data[i].department;
-		let performer = createElem("td").innerHTML = data[i].performer;
-		let view = createElem("td").innerHTML = '<a class="arhive-row__item" data-view="view" href="">Просмотр</a>'
-
-
-
-		row.appendChild("")
-		
+    log(row, archive);
+    archive.appendChild(row);
   }
 }
 
-{
-  /* <tr class="arhive-row">
-			<td><span class="primary">Пойти пожарить шашлиндос с Михалычем</span></td>
-			<td>29.11.2021</td>
-			<td><i class="fas fa-check success"></i></td>
-			<td><a class="arhive-row__item" href="">Рукожопы</a></td>
-			<td><a class="arhive-row__item" href="">Lorem ipsum dolor sit.</a></td>
-			<td><a class="arhive-row__item" data-view="view" href="">Просмотр</i></i></a></td>
-			<td><a class="arhive-row__item" data-view="download" href=""><i class="fa fa-download"></i></a></td>
-</tr> */
+// '<td><span class="' +
+// obj.priority +
+// '">' +
+// obj.name +
+// "</span></td> <td>" +
+// obj.date +
+// '</td> <td><i class="fas fa-check success"></i></td> <td><a class="arhive-row__item" href="">' +
+// obj.department +
+// '</a></td> <td><a class="arhive-row__item" href="">' +
+// obj.performer +
+// '</a></td> <td><a class="arhive-row__item" data-id="' +
+// obj.id +
+// '" data-view="view" href="">Просмотр</a></td> <td><a class="arhive-row__item" data-id="${obj.id}" data-view="download" href=""><i class="fa fa-download"></i></a></td>';
+
+/*{
+  "id": 3,
+    "creationDate": "Apr 12, 2020",
+    "modificationDate": "Apr 12, 2020",
+    "name": "filename_1",
+    "path": "/home/kolmogorov/Java_Practice/bcrew/doc_turnover/src/main/webapp/archive/department__performer__15_04_2020.docx",
+    "performer": {
+  "id": 1,
+      "mame": ""
+      "department": {
+    "id": 3,
+        "departmentName": "sample3",
+        "parentDepartment": "sample_parent3"
+  }
+},
+  "deadline": false,
+    "significance": 0
+},*/
+
+
+$("body").on("click", function (e) {
+  let elem = e.target;
+  log(elem.classList.contains("profile_bar-btn"));
+  if (
+      !elem.classList.contains("profile_bar-btn") &&
+      !elem.parentElement.classList.contains("profile_bar-btn")
+  ) {
+    $(".profile_bar-btn").removeClass("active");
+    $(".settings").fadeOut(100);
+  }
+});
+
+function profileDDmenu() {
+  let btn = $(".profile_bar-btn");
+  let content = $(".settings");
+
+  setTimeout(function () {
+    if (!btn.hasClass("active")) {
+      content.fadeIn(100);
+      btn.addClass("active");
+    } else {
+      content.fadeOut(100);
+      btn.removeClass("active");
+    }
+  }, 0);
 }
+
+// todo list
+
+let todos = $(".todo_item");
+let todos_noActive = selector("#todos-no-active")
+let todos_active = selector("#todos-active");
+let todo;
+
+function todoDragStart() {
+  $(this).fadeOut(300);
+  todo = this;
+}
+
+function todoDragEnd() {
+  if(!$(document.body).hasClass("modal-open")){
+    $(this).fadeIn();
+  };
+  log("end");
+}
+
+function todoDrop(e) {
+  $('#modalAddTodo').modal(open);
+
+  selector("#addTodoBtn").onclick = () => {
+    this.appendChild(todo);
+    $(todo).fadeIn();
+  }
+  $('#modalAddTodo').on('hide.bs.modal', function (e) {
+    if(todo.parentElement.getAttribute("id", "todos-no-active") ) {
+      $(todo).fadeIn();
+    };
+  })
+}
+
+todos.each((i, todo) => {
+  todo.addEventListener("dragstart", todoDragStart);
+
+  todo.addEventListener("dragend", todoDragEnd);
+});
+
+todos_active.addEventListener("dragover", (e) => {
+  e.preventDefault();
+});
+
+todos_active.addEventListener("dragenter", (e) => {
+  e.preventDefault();
+});
+
+todos_active.addEventListener("drop", todoDrop);
